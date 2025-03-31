2b7bdea4d1be318a7906df16d14bd91f89121318
RupakBoral <boralrupak@gmail.com>
2025-04-01 00:40:08 +0530

    deployment changes

--- backend/email.js
+++ backend/email.js
@@ -4,14 +4,16 @@ const cors = require("cors");
 
 const EmailRouter = express.Router();
 
-EmailRouter.use(express.json());
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+EmailRouter.post("/", (req, res) => {
+  res.send("hello");
 });
 
 EmailRouter.post("/send-email", async (req, res) => {

3df2158bf63a639e64a52120d84ff0bec2065bf7
RupakBoral <boralrupak@gmail.com>
2025-03-31 20:10:27 +0530

    Added backend folder to existing branch

---
+++ backend/email.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const EmailRouter = express.Router();
+
+EmailRouter.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+EmailRouter.post("/send-email", async (req, res) => {
+  const { name, message, fromEmail, phone } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL,
+    subject: "Visitor Information Submission",
+    text: `This mail has been sent to reach out to you.\nThe details of the recruiter is mentioned below:\nName: ${name}\nEmail: ${fromEmail}\nPhone no: ${phone}\n\Message: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error sending email", details: error.message });
+  }
+});
+
+module.exports = EmailRouter;
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_BASE_URL, process.env.DEPLOYED_URL],
+    credentials: true,
+    methods: "GET,POST",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+const EmailRouter = require("./email");
+
+app.use("/", EmailRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
